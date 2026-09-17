0842bbc7da6131ff569e1374f53e3803e4da7184
Pedro Henrique SIlva Rodrigues <ph8464803@gmail.com>
2026-09-17 10:43:24 -0300

    fix: create entity

--- src/cliente/entities/cliente.entity.ts
+++ src/cliente/entities/cliente.entity.ts
@@ -1,1 +1,9 @@
-export class Cliente {}
+import { Cliente as PrismaCliente } from '@prisma/client';
+
+export class Cliente implements PrismaCliente {
+  id: string;
+  nome: string;
+  telefone: string;
+  email: string | null; 
+  dataCadastro: Date;
+}
